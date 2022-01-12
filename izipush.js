5b310c37a71356c9df3a72b265601861a6a95047
naanhidayatz <97573920+naanhidayatz@users.noreply.github.com>
2022-01-12 10:58:14 +0800

    Add files via upload

---
+++ izipush.js
@@ -0,0 +1,1 @@
+<script>(function(d,z,s){s.src='//'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('abazelfan.com',4788824,document.createElement('script'))</script>
